4f991eef3ade9642b6dfd1f82dfa81b6f66c785b
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2024-09-21 14:41:51 -0700

    multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
 number = int(input("Enter a number to see its multiplication table: "))
 for i in range(1, 11):
-    print(str(number) + " * " + str(i) + " = " + str(number * i))
+    result = number * i
+    print(f"{number} * {i} = {result}")
