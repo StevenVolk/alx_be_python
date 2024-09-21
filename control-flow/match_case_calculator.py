e397578bb56b99af3a4a5751de73c7339f03d302
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2024-09-21 10:59:36 -0700

    match_case_calculator.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+op = input("Choose the operation (+, -, *, /): ")
+if op == "+":
+    print("The result is " + str(num1 + num2) + ".")
+elif op == "-":
+    print("The result is " + str(num1 - num2) + ".")
+elif op == "*":
+    print("The result is " + str(num1 * num2) + ".")
+elif op == "/":
+    if num2 == 0:
+        print("Cannot divide by zero.")
+    else:
+        print("The result is " + str(num1 /num2) + ".")
