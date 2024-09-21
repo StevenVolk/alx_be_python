9a2a3011da90c23174d95a88db73436c67b980db
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2024-09-21 12:06:19 -0700

    daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,18 +2,21 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 sensitivity = input("Is it time-bound? (yes/no): ")
-if priority == "high":
-    if sensitivity == "yes":
-        print("Reminder: '" + task + "' is a high priority task that requires immediate attention today!")
-    elif sensitivity == "no":
-        print("Note: '" + task + "' is a high priority task. Consider completing it when you have free time.")
-elif priority == "medium":
-    if sensitivity == "yes":
-        print("Reminder: '" + task + "' is a medium priority task that requires attention today!")
-    elif sensitivity == "no":
-        print("Note: '" + task + "' is a medium priority task. Consider completing it when you have free time.")
-elif priority == "low":
-    if sensitivity == "yes":
-        print("Reminder: '" + task + "' is a low priority task that requires attention today!")
-    elif sensitivity == "no":
-        print("Note: '" + task + "' is a low priority task. Consider completing it when you have free time.")
+match priority:
+    case "high":
+        if sensitivity == "yes":
+            print("Reminder: '" + task + "' is a high priority task that requires immediate attention today!")
+        elif sensitivity == "no":
+            print("Note: '" + task + "' is a high priority task. Consider completing it when you have free time.")
+    case "medium":
+        if sensitivity == "yes":
+            print("Reminder: '" + task + "' is a medium priority task that requires attention today!")
+        elif sensitivity == "no":
+            print("Note: '" + task + "' is a medium priority task. Consider completing it when you have free time.")
+    case "low":
+        if sensitivity == "yes":
+            print("Reminder: '" + task + "' is a low priority task that requires attention today!")
+        elif sensitivity == "no":
+            print("Note: '" + task + "' is a low priority task. Consider completing it when you have free time.")
+    case _:
+        print("invalid")
