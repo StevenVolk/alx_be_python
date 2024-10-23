d2dae4a23b416cd0765121c0eebb18e0d636b055
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2024-10-23 14:28:17 -0700

    polymorphism_demo.py

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,26 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def __str__(self):
+        return(f"The area of the Rectangle is: {Rectangle.area()}")
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (radius ** 2)
+
+    def __str__(self):
+        return(f"The area of the Circle is: {Circle.area()}")
