330e995181b11d41912145738865f08211164e76
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2024-10-23 04:34:00 -0700

    bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,14 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(amount):
+        self.account_balance += amount
+
+    def withdraw(amount):
+        if amount > account_balance:
+            return False
+        return True
+
+    def display_balance():
+        return account_balance
