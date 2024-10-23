3a273440723bc94f1f579d478df92afb91a42c58
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2024-10-23 06:06:17 -0700

    library_management.py

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,26 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.add(book)
+    
+    def check_out_book(self, title):
+        for book in self._books:
+            if title == book.title:
+                self.book._is_checked_out = True
+
+    def return_book(self, title):
+        for book in self._books:
+            if title == book.title:
+                self.book._is_checked_out = False
+
+    def list_available_books():
+        for book in books:
+            print("{book.title} by {book.author}")
